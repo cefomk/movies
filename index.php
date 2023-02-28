1e55f7486c93b6fbeec0409ad24dbc9b271d8518
Your Name <you@example.com>
2023-02-28 16:23:02 +0100

    Liste des 30 premiers films

--- index.php
+++ index.php
@@ -3,11 +3,22 @@
 require './inc/fonctions.php';
 require './inc/pdo.php';
 
-$sqlRequest = "SELECT * FROM movies_full WHERE title like '%vadrouille%'";
-// $sqlRequest = "SELECT * FROM movies_full ";
+// $sqlRequest = "SELECT * FROM movies_full WHERE title like '%vadrouille%'";
+$sqlRequest = "SELECT * FROM movies_full LIMIT 30";
 
 $resultat = $conn->prepare($sqlRequest);
 $resultat->execute();
 $films = $resultat->fetchAll();
 
-dd($films);
+// dd($films);
+$i=0;
+foreach($films as $film)
+{
+    echo $i . '<br>';
+    echo $film['title'] . '<br>';
+    echo $film['genres'] . '<br>';
+    echo $film['cast'] . '<br>';
+    echo $film['plot'] . '<br>';
+    echo '<hr>';
+    $i++;
+}
